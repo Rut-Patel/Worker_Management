5703b5f258964ccce7c25b3ddfb68f09a2238ba3
Rut Patel <patelrut238@gmail.com>
2020-12-07 18:09:34 -0500

    Commenting the Code

--- Startup.cs
+++ Startup.cs
@@ -41,7 +41,7 @@ namespace Worker_Management
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {    
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
